91c63f0b3179fc9257dbf05d6a6414a2864ede0a
Richard Dowden <richard.dowden@purpletonic.com>
2011-05-17 23:36:47 +0100

    Calculations to get distance in KM

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,51 @@
 // Put your application scripts here  
  
 window.onload = function(){
+
+  // http://groups.google.com/group/google-maps-js-api-v3/browse_thread/thread/3a9f3b83b941a2d7/7695b14e1dbc4f1c
+  google.maps.LatLng.prototype.kmTo = function(a){ 
+    var e = Math, ra = e.PI/180; 
+    var b = this.lat() * ra, c = a.lat() * ra, d = b - c; 
+    var g = this.lng() * ra - a.lng() * ra; 
+    var f = 2 * e.asin(e.sqrt(e.pow(e.sin(d/2), 2) + e.cos(b) * e.cos 
+(c) * e.pow(e.sin(g/2), 2))); 
+    return f * 6378.137; 
+  } 
+
+  google.maps.Polyline.prototype.inKm = function(n){ 
+    var a = this.getPath(n), len = a.getLength(), dist = 0; 
+    for(var i=0; i<len-1; i++){ 
+      dist += a.getAt(i).kmTo(a.getAt(i+1)); 
+    } 
+    return dist; 
+  } 
+
+  // http://stackoverflow.com/questions/2698112/how-to-add-markers-on-google-maps-polylines-based-on-distance-along-the-line
+  google.maps.Polyline.prototype.Distance = function() {
+    var dist = 0;
+    for (var i=1; i < this.getPath().getLength(); i++) {
+      dist += this.getPath().getAt(i).distanceFrom(this.getPath().getAt(i-1));
+    }
+    return dist;
+  }
+
+  google.maps.LatLng.prototype.distanceFrom = function(newLatLng) {
+    var R = 6371; // km (change this constant to get miles)
+    //var R = 6378100; // meters
+    var lat1 = this.lat();
+    var lon1 = this.lng();
+    var lat2 = newLatLng.lat();
+    var lon2 = newLatLng.lng();
+    var dLat = (lat2-lat1) * Math.PI / 180;
+    var dLon = (lon2-lon1) * Math.PI / 180;
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) *
+      Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    return d;
+  }
+
   var map;
   var london = new google.maps.LatLng(51.527382,-0.098662);
   //http://maps.google.co.uk/maps?q=london&ie=UTF8&hq=&hnear=Westminster,+London,+United+Kingdom&gl=uk&ll=&spn=0.247276,0.451126&z=12&iwloc=A
@@ -173,8 +218,10 @@ window.onload = function(){
 
   // console.info(marathonRoute.length);
 
-  var plotMarathonRoute = [];
+  //26.2
+  console.info(Math.round(marathonRoute.length / 26.2))
 
+  var plotMarathonRoute = [];
   $(marathonRoute).each(function(){
     plotMarathonRoute.push(
       new google.maps.LatLng(this[0],this[1])
@@ -214,4 +261,6 @@ window.onload = function(){
 
   // var courseLine = new google.maps.Polyline(courseOpts);
   var fundLine = new google.maps.Polyline(lineOpts);
+  console.info(fundLine.inKm());
+  console.info(fundLine.Distance());
 }
