0361c0091861709f681aef018a3a8a8560ae2057
Richard Dowden <richard.dowden@purpletonic.com>
2011-06-11 15:12:50 +0100

    Plot a marker on the map every KM

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,4 @@
-// // Put your application scripts here  
- 
-// window.onload = function(){
-
-//   // http://groups.google.com/group/google-maps-js-api-v3/browse_thread/thread/3a9f3b83b941a2d7/7695b14e1dbc4f1c
+  // http://groups.google.com/group/google-maps-js-api-v3/browse_thread/thread/3a9f3b83b941a2d7/7695b14e1dbc4f1c
 //   google.maps.LatLng.prototype.kmTo = function(a){ 
 //     var e = Math, ra = e.PI/180; 
 //     var b = this.lat() * ra, c = a.lat() * ra, d = b - c; 
@@ -20,66 +16,52 @@
 //     return dist; 
 //   } 
 
-//   // http://stackoverflow.com/questions/2698112/how-to-add-markers-on-google-maps-polylines-based-on-distance-along-the-line
-//   google.maps.Polyline.prototype.Distance = function() {
-//     var dist = 0;
-//     for (var i=1; i < this.getPath().getLength(); i++) {
-//       dist += this.getPath().getAt(i).distanceFrom(this.getPath().getAt(i-1));
-//     }
-//     return dist;
-//   }
-
-//   google.maps.LatLng.prototype.distanceFrom = function(newLatLng) {
-//     var R = 6371; // km (change this constant to get miles)
-//     //var R = 6378100; // meters
-//     var lat1 = this.lat();
-//     var lon1 = this.lng();
-//     var lat2 = newLatLng.lat();
-//     var lon2 = newLatLng.lng();
-//     var dLat = (lat2-lat1) * Math.PI / 180;
-//     var dLon = (lon2-lon1) * Math.PI / 180;
-//     var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-//       Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) *
-//       Math.sin(dLon/2) * Math.sin(dLon/2);
-//     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-//     var d = R * c;
-//     return d;
-//   }
-
-//   // console.info(marathonRoute.length);
-//   //26.2
-//   console.info("Number of markers per mile: " + Math.round(marathonRoute.length / 26.2));
-
-//   //Complete Route
-//   var plotMarathonRoute = [];
-//   $(marathonRoute).each(function(){
-//     plotMarathonRoute.push(
-//       new google.maps.LatLng(this[0],this[1])
-//     )
-//   });
-
-//   //Completed Route
-//   for(point in marathonRoute){
-
-//   }
-
-//   var lineOpts = { 
-//     path: plotMarathonRoute,
-//     map: map,
-//     strokeColor: "#FFFFFF",
-//     strokeOpacity: 0.5,
-//     strokeWeight: 10
-//   };
-
-//   // Plot the complete route
-//   var fundLine = new google.maps.Polyline(lineOpts);
-  
-//   console.info(fundLine.inKm());
-//   console.info(fundLine.Distance());
-// }
-
-
 $(document).ready(function(){
+
+  // Extend google maps with distance funcitons
+  // http://stackoverflow.com/questions/2698112/how-to-add-markers-on-google-maps-polylines-based-on-distance-along-the-line
+  google.maps.Polyline.prototype.Distance = function() {
+    var dist = 0;
+    for (var i=1; i < this.getPath().getLength(); i++) {
+      dist += this.getPath().getAt(i).distanceFrom(this.getPath().getAt(i-1));
+    }
+    return dist;
+  }
+
+  google.maps.LatLng.prototype.distanceFrom = function(newLatLng) {
+    var R = 6371; // km (change this constant to get miles)
+    //var R = 6378100; // meters
+    var lat1 = this.lat();
+    var lon1 = this.lng();
+    var lat2 = newLatLng.lat();
+    var lon2 = newLatLng.lng();
+    var dLat = (lat2-lat1) * Math.PI / 180;
+    var dLon = (lon2-lon1) * Math.PI / 180;
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) *
+      Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    return d;
+  }
+
+  // Every 1km place a marker
+  google.maps.Polyline.prototype.KmMarkers = function() {
+    var dist = 0;
+    var markers = [];
+    for (var i=1; i < this.getPath().getLength(); i++) {
+      dist += this.getPath().getAt(i).distanceFrom(this.getPath().getAt(i-1));
+
+      if(dist >= 1.0){
+        markers.push(this.getPath().getAt(i));
+        dist = 0;
+      }
+    }
+    return markers;
+  }
+
+
+
   // Get the fund raising data from JG
   var efforts = $.parseJSON($.ajax({
       url: "/fundraising_status.json",
@@ -252,6 +234,23 @@ $(document).ready(function(){
   };
   // Plot the complete route
   var completedRoute = new google.maps.Polyline(completedRouteLineOpts);
+
+
+  // Add markers to the map
+  // console.info(marathonRoute.length);
+  // Marathon length in miles = 26.2
+  // console.info("Number of markers per mile: " + Math.round(marathonRoute.length / 26.2));
+  // console.info("Number of array per km: " + Math.round(marathonRoute.length / 26.2));
+  $(completeRoute.KmMarkers()).each(function(){
+    // console.info(this);
+    var current_marker = new google.maps.Marker({
+      position: this,
+      map: map,
+    });
+  });
+
+  //test = completeRoute.KmMarkers();
+  //console.info(typeof(test[0]));
 
   // Set the maps center to be the current percentage
   map.setCenter(new google.maps.LatLng(marathonRoute[current_location][0],marathonRoute[current_location][1]));
